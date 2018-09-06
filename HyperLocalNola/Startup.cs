e7e9e7ba6086d9ae9cb464bb89d323d65640c414
nickassembly <nickg@maliciousgames.com>
2018-09-06 18:03:09 -0500

    Reconfigure routing

--- HyperLocalNola/Startup.cs
+++ HyperLocalNola/Startup.cs
@@ -57,7 +57,7 @@ namespace HyperLocalNola
          app.UseMvc(routes => {
             routes.MapRoute(
        name: "default",
-       template: "{controller=Home}/{action=Index}/{id?}");
+       template: "{controller=App}/{action=Index}/{id?}");
          });
       }
    }
